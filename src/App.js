851e3d5e75bf6710a4b20ea9bea45f3ab31303ad
github username <github email address>
2020-01-23 14:23:57 -0600

    resolved app.js typo

--- src/App.js
+++ src/App.js
@@ -13,20 +13,12 @@ class App extends React.Component {
   };
 
   handleLogin = user => {
-<
     if (user.jwt) {
-
       this.setState({
         loggedIn: true,
         currentName: user.user.user.name,
         currentId: user.user.user.id
       });
-
-   
-  };
-
- 
-
     } else {
       alert("Invalid email or password");
     }
